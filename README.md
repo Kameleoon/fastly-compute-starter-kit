5e11dfbaf38b53de9e74ea5afb5058f0a5922710
ezheenchoroev <122873039+ezheenchoroev@users.noreply.github.com>
2023-01-17 16:37:46 +0600

    feat: fastly implementation (#1)
    * initial commit
    
    * feat: add backend setups
    
    * feat: fetch config data file from cdn
    
    * feat: add js sdk integration
    
    * feat: add kameleoonClient module
    
    * docs: write an instruction
    
    * fix: set proper authors email

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# fastly-compute-starter-kit
+# Kameleoon + Fastly Compute@Edge Starter Kit
+
+Starter Kit to run Kameleoon Experimentation and Feature Flags on Fastly Compute@Edge service.
+
+## How to use
+
+### Before Get started
+
+These steps should be completed before you get started:
+
+1. Have an account in Fastly. If you don't have it please go to [fastly registration page](https://www.fastly.com/signup) and register. For additional information go Compute@Edge [documentation](https://developer.fastly.com/learning/compute).
+2. Install [Fastly CLI](https://developer.fastly.com/learning/tools/cli).
+3. Have an account in Kameleoon.
+
+### Get started
+
+1. Create a folder and inside this folder initialize Fastly Compute@Edge service using [Fastly CLI](https://developer.fastly.com/reference/cli).
+
+```sh
+fastly compute init --from https://github.com/Kameleoon/fastly-compute-starter-kit
+```
+
+2. Follow the steps, fill up service name, description and other information. Add your `service_id` to `fastly.toml` file. If you don't have an existing service, new service will be created.
+
+3. Install modules
+
+```sh
+npm install
+```
+
+4. Build and deploy:
+
+```sh
+fastly compute publish
+```
+
+5. Monitor logs:
+
+```sh
+fastly log-tails
+```
+
+## Additional resources
+
+- [Fastly Compute@Edge Documentation](https://docs.fastly.com/products/compute-at-edge)
+- [JavaScript on Compute@Edge](https://developer.fastly.com/learning/compute/javascript)
+- [Kameleoon JS SDK Documentation](https://developers.kameleoon.com/javascript-sdk.html)
---
+++ src/index.js
@@ -0,0 +1,50 @@
+/// <reference types="@fastly/js-compute" />
+import { KameleoonClient } from "kameleoon-client-javascript";
+import cookie from "cookie";
+import { v4 } from "uuid";
+import { getConfigDataFile } from "./helpers";
+
+const KAMELEOON_USER_ID = "kameleoon_user_id";
+
+addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
+
+async function handleRequest(event) {
+  const cookies = cookie.parse(event.request.headers.get("Cookie") || "");
+
+  // Fetch user id from the cookie if available to make sure that results are sticky.
+  const visitorCode = cookies[KAMELEOON_USER_ID] || v4();
+
+  // Fetch config file from Kameleoon CDN and cache it using Fastly for given number of seconds
+  // Get the siteCode from Kameleoon Platform
+  const configDataFile = await getConfigDataFile("<YOUR_SITE_CODE>", 600);
+  const parsedConfigDataFile = JSON.parse(configDataFile);
+
+  // Initialize the KameleoonClient
+  const kameleoonClient = new KameleoonClient(
+    // Get the siteCode from Kameleoon Platform
+    "<YOUR_SITE_CODE>",
+    // Add the necessary configurations. For example {environment: production}
+    {},
+    /***
+     * @param configDataFile - Fetched and cached configDataFile from cdn
+     * @param fetchCallback - Fetch API which is used in our sdk to fetch data and perform a data tracking and it should be passed always along with configDataFile
+     */
+    { configDataFile: parsedConfigDataFile, fetchCallback: fetch }
+  );
+
+  // Use kameleoonClient instance to access SDK methods
+  const variationKey = kameleoonClient.getFeatureVariationKey(
+    visitorCode,
+    "<YOUR_FEATURE_KEY>"
+  );
+  console.log(`The variationKey is ${variationKey}`);
+
+  let headers = new Headers();
+  headers.set("Content-Type", "text/plain");
+  headers.set("Set-Cookie", cookie.serialize(KAMELEOON_USER_ID, visitorCode));
+
+  return new Response(
+    "Welcome to Kameleoon Starter Kit, use 'fastly log-tail' command for results",
+    { status: 200, headers }
+  );
+}
